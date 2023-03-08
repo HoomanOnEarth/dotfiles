7e2e4c0afc730bad0d260f283bf2b606013b7d7a
0xlkda <lekhacduyanh@gmail.com>
2023-03-09 04:57:30 +0700

    ignore logs

--- nvim/init.lua
+++ nvim/init.lua
@@ -129,31 +129,6 @@ require("lazy").setup({
 		end,
 	},
 
-	-- Terminals
-	{
-		"akinsho/toggleterm.nvim",
-		config = function()
-			require("toggleterm").setup({
-				shade_terminals = false,
-				open_mapping = "<M-`>",
-				direction = "vertical",
-				size = 80,
-			})
-
-			function _G.set_terminal_keymaps()
-				local opts = { buffer = 0 }
-				map("t", "<ESC>", [[<C-\><C-n>]], opts)
-				map("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-				map("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-				map("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-				map("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-			end
-
-			-- if you only want these mappings for toggle term use term://*toggleterm#* instead
-			vim.cmd("autocmd TermOpen term://* lua set_terminal_keymaps()")
-		end,
-	},
-
 	-- Syntax
 	"sheerun/vim-polyglot",
 
